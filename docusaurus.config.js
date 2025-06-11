a10bac30e6473d63101986383b27dbc6cff0998f
JoniPeti018 <jonaspeter018@gmail.com>
2025-06-11 16:10:29 +0200

    Github Actions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,9 +18,9 @@ const config = {
   url: 'https://JoniPeti018.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  //baseUrl: '/',
   // GitHub pages deployment config.
-  // baseUrl: '/Demo/',
+  baseUrl: '/Demo/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
